b79c0f1eceb77a6128f71af0cd93373101c107f9
pix <oblepixa1999@mail.ru>
2022-01-18 15:44:24 +0300

    last

---
+++ Notes/Notes/Views/AboutPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Notes.Models;
+using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("https://translated.turbopages.org/proxy_u/en-ru.ru.5b7de23c-61e68208-3015a547-74722d776562/https/en.wikipedia.org/wiki/Note-taking");
+        }
+
+    }
+}
